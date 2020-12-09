4700017635fbfa9c77235d7269b5fffc2a86ed10
Busoi Andreea <andreea.busoi@outlook.com>
2020-12-09 17:05:51 +0200

    Adaugare entitate Utilizator

---
+++ Utilizator.js
@@ -0,0 +1,23 @@
+import Sequelize from 'sequelize';
+import sequelize from '../dbConfig.js';
+
+const Utilizator = sequelize.define("Utilizator", {
+    UtilizatorId:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+
+    EmailAddress:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    Parola:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+export default Utilizator;
